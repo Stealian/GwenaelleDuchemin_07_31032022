3dc2c898a6a229c27daeb59a662cee35dc441a05
Stealian <gwenaelle.duchemin.pat@gmail.com>
2022-04-30 21:36:22 +0200

    réjustement

--- database/socialnetwork.sql
+++ database/socialnetwork.sql
@@ -44,7 +44,7 @@ CREATE TABLE `comments` (
 
 LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-INSERT INTO `comments` VALUES (3,'Ok chef !','Cyril','2020-10-27 14:26:37','2020-10-27 14:26:37',10,1),(4,'yes','Marco','2020-10-27 14:27:39','2020-10-27 14:27:39',11,1),(5,'trop chou !','Estelle','2020-10-27 14:29:08','2020-10-27 14:29:08',12,6),(6,'Toujours 😃','Estelle','2020-10-27 14:29:28','2020-10-27 14:29:28',12,1),(7,'je préfère ça ahah','admin','2020-10-27 14:30:56','2020-10-27 14:30:56',1,1);
+INSERT INTO `comments` VALUES (3,'Ok chef !','Cyril','2022-04-05 14:26:37','2022-04-05 14:26:37',10,1),(4,'yes','Marco','2022-04-05 14:27:39','2022-04-05 14:27:39',11,1),(5,'trop chou !','Estelle','2022-04-05 14:29:08','22022-04-05 14:29:08',12,6),(6,'Toujours 😃','Estelle','2022-04-05 14:29:28','2022-04-05 14:29:28',12,1),(7,'je préfère ça ahah','admin','2022-04-05 14:30:56','2022-04-05 14:30:56',1,1);
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -75,7 +75,7 @@ CREATE TABLE `likes` (
 
 LOCK TABLES `likes` WRITE;
 /*!40000 ALTER TABLE `likes` DISABLE KEYS */;
-INSERT INTO `likes` VALUES (4,'2020-10-27 14:26:32','2020-10-27 14:26:32',10,1),(5,'2020-10-27 14:27:32','2020-10-27 14:27:32',11,1),(6,'2020-10-27 14:27:42','2020-10-27 14:27:42',11,5),(7,'2020-10-27 14:28:57','2020-10-27 14:28:57',12,6),(8,'2020-10-27 14:29:13','2020-10-27 14:29:13',12,1),(9,'2020-10-27 14:31:04','2020-10-27 14:31:04',1,6);
+INSERT INTO `likes` VALUES (4,'2022-04-05 14:26:32','2022-04-05 14:26:32',10,1),(5,'2022-04-05 14:27:32','2022-04-05 14:27:32',11,1),(6,'2022-04-05 14:27:42','2022-04-05 14:27:42',11,5),(7,'2022-04-05 14:28:57','2022-04-05 14:28:57',12,6),(8,'2022-04-05 14:29:13','2022-04-05 14:29:13',12,1),(9,'2022-04-05 14:31:04','2022-04-05 14:31:04',1,6);
 /*!40000 ALTER TABLE `likes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,7 +106,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (1,'Bienveillance et respect svp 😊',NULL,NULL,'2020-10-27 13:56:34','2020-10-27 14:26:14',1),(5,'😁','https://media.giphy.com/media/3xz2BtNxiBNTisHygM/giphy.gif',NULL,'2020-10-27 14:27:00','2020-10-27 14:27:09',10),(6,'Hello','https://media1.giphy.com/media/m8SQE8720OkEg/giphy.webp?cid=ecf05e47v2zsgqoae9u5sphgxnvuqbkc4344wbowrl56mijn&rid=giphy.webp',NULL,'2020-10-27 14:27:56','2020-10-27 14:28:29',11),(7,'Je vous surveille 😉','https://media2.giphy.com/media/fGFpff9dvV9LO/giphy.webp?cid=ecf05e47d401mllsp4rzpmqpxmxr3i16noq119jemrp0j7ng&rid=giphy.webp',NULL,'2020-10-27 14:30:33','2020-10-27 14:30:33',1);
+INSERT INTO `posts` VALUES (1,'Bienveillance et respect svp 😊',NULL,NULL,'2022-04-05 13:56:34','2022-04-05 14:26:14',1),(5,'😁','https://media.giphy.com/media/3xz2BtNxiBNTisHygM/giphy.gif',NULL,'2022-04-05 14:27:00','2022-04-05 14:27:09',10),(6,'Hello','https://media1.giphy.com/media/m8SQE8720OkEg/giphy.webp?cid=ecf05e47v2zsgqoae9u5sphgxnvuqbkc4344wbowrl56mijn&rid=giphy.webp',NULL,'2022-04-05 14:27:56','2022-04-05 14:28:29',11),(7,'Je vous surveille 😉','https://media2.giphy.com/media/fGFpff9dvV9LO/giphy.webp?cid=ecf05e47d401mllsp4rzpmqpxmxr3i16noq119jemrp0j7ng&rid=giphy.webp',NULL,'2022-04-05 14:30:33','2022-04-05 14:30:33',1);
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -138,7 +138,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin','admin@mail.com','$2b$10$ullNHu9dYYSjH0ioi7SpyemMNhW7vdIZ6aGBw.D7.bxArp58J8Vdq','http://localhost:3000/upload/profil_admin1603808637091.png',NULL,1,'2020-10-27 13:56:03','2020-10-27 14:23:57'),(10,'Cyril','cyril@mail.com','$2b$10$KvD1o7KWzv5hksCQV5VqveWgZuxh5rZOAQ5YCdBN90hxZDq9PEUBm',NULL,NULL,0,'2020-10-27 14:26:25','2020-10-27 14:26:25'),(11,'Marco','marc@mail.com','$2b$10$Fu.apmLEqIm5uvMHe12adO595rnlnojNWk38W3VSMazlF8EIyVoRW',NULL,NULL,0,'2020-10-27 14:27:25','2020-10-27 14:27:25'),(12,'Estelle','estelle@mail.com','$2b$10$Vbkq3b4j97sbGuBj0ayS2.vDwFM9VgA6nzOMdI4pnQnzqpOurgkMe',NULL,NULL,0,'2020-10-27 14:28:53','2020-10-27 14:28:53');
+INSERT INTO `users` VALUES (1,'admin','admin@mail.com','$2b$10$ullNHu9dYYSjH0ioi7SpyemMNhW7vdIZ6aGBw.D7.bxArp58J8Vdq','http://localhost:3000/upload/profil_admin1603808637091.png',NULL,1,'2022-04-05 13:56:03','2022-04-05 14:23:57'),(10,'Cyril','cyril@mail.com','$2b$10$KvD1o7KWzv5hksCQV5VqveWgZuxh5rZOAQ5YCdBN90hxZDq9PEUBm',NULL,NULL,0,'2022-04-05 14:26:25','2022-04-05 14:26:25'),(11,'Marco','marc@mail.com','$2b$10$Fu.apmLEqIm5uvMHe12adO595rnlnojNWk38W3VSMazlF8EIyVoRW',NULL,NULL,0,'2022-04-05 14:27:25','2022-04-05 14:27:25'),(12,'Estelle','estelle@mail.com','$2b$10$Vbkq3b4j97sbGuBj0ayS2.vDwFM9VgA6nzOMdI4pnQnzqpOurgkMe',NULL,NULL,0,'2022-04-05 14:28:53','2022-04-05 14:28:53');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -151,4 +151,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-27 15:31:56
+-- Dump completed on 2022-04-05 15:31:56
